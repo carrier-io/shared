5418d2401c9ea1a8416c7ce8fd965dc168ad1bf7
Aspect13 <ad13box@gmail.com>
2023-09-26 19:10:13 +0400

    removing serialization warning

--- tools/db_tools.py
+++ tools/db_tools.py
@@ -41,7 +41,7 @@ class AbstractBaseMixin:
         return json.dumps(self.to_json(), indent=2)
 
     def to_json(self, exclude_fields: tuple = ()) -> dict:
-        log.warning('Be cautious "to_json()". Better write your own serialization for %s', getattr(self, '__tablename__'))
+        # log.warning('Be cautious "to_json()". Better write your own serialization for %s', getattr(self, '__tablename__'))
         result = dict()
         for column in self.__table__.columns:
             if column.name not in exclude_fields:
